a2dac43c2098d08357a6c2bafa0e175b970a8109
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2022-07-29 09:18:02 +0300

    Header and Footer with correct Wheel Run logos

--- components/layout/footer/footer.tsx
+++ components/layout/footer/footer.tsx
@@ -48,15 +48,8 @@ export const Footer = ({ data, icon, rawData }) => {
         <div className="flex justify-between items-center gap-6 flex-wrap">
           <Link href="/" passHref>
             <a className="group mx-2 flex items-center font-bold tracking-tight text-gray-400 dark:text-gray-300 opacity-50 hover:opacity-100 transition duration-150 ease-out whitespace-nowrap">
-              <Icon
-                parentColor={data.color}
-                data={{
-                  name: icon.name,
-                  color: data.color === "primary" ? "primary" : icon.color,
-                  style: icon.style,
-                }}
-                className="inline-block h-10 w-auto group-hover:text-orange-500"
-              />
+              <img src="https://s3.eu-west-1.amazonaws.com/wheelrun.biz-public/app-icon-512x512-300dpi.png" alt="Wheel Run Logo"
+                   className="max-w-[42px] rounded-full"/>
             </a>
           </Link>
           <div className="flex gap-4">
