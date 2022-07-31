4217c2b79215a7cbe63807b3f2f80f676c9c0e36
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2022-07-31 17:44:51 +0300

    Favicon.ico

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -31,6 +31,12 @@ export const Layout = ({ rawData = {}, data = layoutData, children }) => {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta name="description" content={headDescription} />
 
+        {/* ------- favicon ----------------------*/}
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="manifest" href="/site.webmanifest"></link>
+
         {/* ------- Open Graph Protocol ----------*/}
         <meta property="og:type" content={ogType} />
         <meta property="og:title" content={headTitle} />
