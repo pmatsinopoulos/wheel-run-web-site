e0f9e9211d9f3eceb7d3e2eb1aaa6acd78a3c416
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2022-08-03 00:33:42 +0300

    Posts index page uses a Hero block to display a hero section

--- pages/posts.tsx
+++ pages/posts.tsx
@@ -2,6 +2,7 @@ import { Container } from "../components/util/container";
 import { Section } from "../components/util/section";
 import { Posts } from "../components/posts";
 import { ExperimentalGetTinaClient } from "../.tina/__generated__/types";
+import { useTina } from "tinacms/dist/edit-state";
 import { Layout } from "../components/layout";
 import { Hero } from "../components/blocks/hero";
 
@@ -9,6 +10,12 @@ export default function HomePage(
   props: AsyncReturnType<typeof getStaticProps>["props"]
 ) {
   const posts = props.data.postConnection.edges;
+
+  const { data } = useTina({
+    query: props.query,
+    variables: props.variables,
+    data: props.data,
+  });
 
   const layoutData = {
     ...props.data.global,
@@ -18,20 +25,21 @@ export default function HomePage(
         description: 'Tech and Industry Blog Posts',
         og: {
           type: 'website',
-          image: 'https://res.cloudinary.com/oneth0usandwords/image/upload/v1658642896/wheel-run/production/website/shelby_lyvice.jpg',
+          image: 'https://res.cloudinary.com/oneth0usandwords/image/upload/v1659474047/wheel-run/production/website/automobile-1-beatle_gkoetg.jpg',
         },
       },
     },
     header: {
+      ...data.global.header,
       brand: 'Wheel Run',
     },
   };
 
-  const data = {
+  const heroData = {
     id: 'blog-post-hero-section',
     color: "primary",
     image:  {
-      src: 'https://res.cloudinary.com/oneth0usandwords/image/upload/v1659474047/wheel-run/production/website/automobile-1-beatle_gkoetg.jpg',
+      src: layoutData.head.meta.og.image,
       alt: 'Old Beatle Car with Funcy Colors',
     },
     tagline: "Knowledge Base",
@@ -46,7 +54,7 @@ export default function HomePage(
     <Layout data={layoutData as any}>
       <Section className="flex-1">
         <Container size="large">
-          <Hero data={data} parentField={parentField} />
+          <Hero data={heroData} parentField={parentField} />
           <Posts data={posts} />
         </Container>
       </Section>
